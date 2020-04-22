b9654c78d01537d0906e9abf342b342036187847
lmilne360 <lmilne360@gmail.com>
2020-04-22 00:36:11 -0500

    Update password.validator.ts
    replaced optional chaining with AND operator due to stackblitz using outdated version of typescript

--- src/app/validators/password.validator.ts
+++ src/app/validators/password.validator.ts
@@ -8,7 +8,7 @@ import { FormGroup } from '@angular/forms';
 export const  passwordValidator = (control: FormGroup): {[key: string]: boolean } | null => {
   const password = control.get('password');
   const confirmPassword = control.get('confirmPassword');
-   if (password?.pristine || confirmPassword?.pristine) return null;
+   if (password && password.pristine || confirmPassword && confirmPassword.pristine) return null;
 
    return password && confirmPassword && password.value !== confirmPassword.value ? {'misMatch': true} : null;
 }
